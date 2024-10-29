1c3a8d64091631526f037b3c7219ce448fc8db0c
voicanelena <168528814+voicanelena@users.noreply.github.com>
2024-10-29 19:38:21 +0200

    solutie

--- main.cpp
+++ main.cpp
@@ -1,8 +1,14 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <vector>
 
 using namespace std;
+
+vector < pair < string, string >>vec1;
+vector < pair < string, string >>vec2;
+
+
 
 int main()
 {
@@ -10,14 +16,14 @@ int main()
 
     int no_problems, no_doctors;
     string name, speciality;
-    
+
     inFile >> no_problems;
 
     for (int i = 0; i < no_problems; i++)
     {
         inFile >> name;
         inFile >> speciality;
-        cout << name << ' ' << speciality << '\n';
+        vec1.push_back(make_pair(name, speciality));
     }
 
     inFile >> no_doctors;
@@ -26,7 +32,21 @@ int main()
     {
         inFile >> name;
         inFile >> speciality;
-        cout << name << ' ' << speciality << '\n';
+        vec2.push_back(make_pair(name, speciality));
+    }
+
+    for (auto index1 = begin(vec1); index1 != end(vec1); index1++) {
+        bool accepted = false;
+        for (auto index2 = begin(vec2); index2 != end(vec2); index2++) {
+            if ((*index1).second == (*index2).second)
+                accepted = true;
+
+
+        }
+        if (accepted == true)
+            cout << (*index1).first << " " << "Acceptat" << endl;
+        else
+            cout << (*index1).first << " " << "Respins" << endl;
     }
 
     return 0;
