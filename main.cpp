962e46089c6f347fae5eacd7cc47471b168de66c
voicanelena <168528814+voicanelena@users.noreply.github.com>
2024-11-29 21:08:51 +0200

    rezolvare stl3

--- main.cpp
+++ main.cpp
@@ -2,28 +2,64 @@
 #include <fstream>
 #include <string>
 #include <vector>
-
+#include <queue>
 using namespace std;
 
-vector < pair < string, string >>vec1;
-vector < pair < string, string >>vec2;
 
 
+struct Problem {
+    string name;
+    string speciality;
+    int necessary_time;
+    int severity;
+
+    Problem(string& name, string& speciality, int necessary_time,int severity) {
+        this->name = name;
+        this->speciality = speciality;
+        this->necessary_time = necessary_time;
+        this->severity = severity;
+    }
+
+    bool operator<(Problem const other)const {
+        return this->severity < other.severity;
+    }
+};
+
+struct Doctor {
+    string name;
+    string speciality;
+    int time;
+
+    vector<Problem>problems;
+
+    //ca sa asignam din declarare
+    Doctor(string& name, string& speciality, int time) {
+        this->name = name;
+        this->speciality = speciality;
+        this->time = time;
+    }
+
+};
+
+priority_queue <Problem>vec1;
+vector <Doctor>vec2;
 
 int main()
 {
     ifstream inFile("input.txt");
 
-    int no_problems, no_doctors;
+    int no_problems, no_doctors,time,severity;
     string name, speciality;
-
+    
     inFile >> no_problems;
 
     for (int i = 0; i < no_problems; i++)
     {
         inFile >> name;
         inFile >> speciality;
-        vec1.push_back(make_pair(name, speciality));
+        inFile >> time;
+        inFile >> severity;
+        vec1.push(Problem(name,speciality,time,severity));
     }
 
     inFile >> no_doctors;
@@ -32,19 +68,29 @@ int main()
     {
         inFile >> name;
         inFile >> speciality;
-        vec2.push_back(make_pair(name, speciality));
+        vec2.push_back(Doctor(name,speciality,8));
     }
     
-    for (auto problem= begin(vec1);  problem!= end(vec1); problem++) {
-        auto doctor = find_if(vec2.begin(), vec2.end(), [=](pair<string, string>n) {return problem->second == n.second; });
+    while(!vec1.empty()) {
+        auto problem = vec1.top();
+        vec1.pop();
+        auto doctor = find_if(vec2.begin(), vec2.end(), [=](Doctor current) {
+            return problem.speciality == current.speciality && problem.necessary_time <= current.time;});
         
         if (doctor != vec2.end()){
-            cout << doctor->first << " " << problem->first << '\n';
-            vec2.erase(doctor);
+            doctor->time -= problem.necessary_time;
+            doctor->problems.push_back(problem);
         }
    
     }
 
-
+    for (int i = 0; i < no_doctors; i++) {
+        cout << vec2[i].name << " " << vec2[i].problems.size() << " ";
+        for (auto problem : vec2[i].problems) {
+            cout << problem.name << " ";
+        }
+        cout << endl;
+    }
+  
     return 0;
 }
